4d6dbe77a0f2df4909df4e25b31e819912683ef2
rebeccaaaaaaaaaaa <rebeccaignacio1996@gmail.com>
2023-10-24 02:58:56 -0300

    feat: create all functions

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
-createApp(App).mount('#app')
+
+createApp(App)
+.use(VueAxios, axios)
+.mount('#app');
